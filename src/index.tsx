1337c02250d25c54c64376caf41ca0f5815cae56
Thommy Jonasson <thommy.jonasson@gmail.com>
2018-01-18 10:25:18 +0100

    Added react big calendar lib.

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,15 @@ import * as ReactDOM from 'react-dom';
 import {observable} from 'mobx';
 import {observer} from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
+import Calendar from 'react-big-calendar';
+import * as moment from 'moment';
+import events from 'events';
+
+Calendar.setLocalizer(Calendar.momentLocalizer(moment))
 
 class AppState {
     @observable timer = 0;
-
+    @observable Oevents = ['Test', '1', '2'];
     constructor() {
         setInterval(() => {
             this.timer += 1;
@@ -25,7 +30,22 @@ class TimerView extends React.Component<{appState: AppState}, {}> {
             <div>
                 <button onClick={this.onReset}>
                     Seconds passed: {this.props.appState.timer}
-                </button>
+                </button>       
+                <Calendar
+                    selectable
+                    events={events}
+                    defaultView="week"
+                    scrollToTime={new Date(1970, 1, 1, 6)}
+                    defaultDate={new Date(2015, 3, 12)}
+                    onSelectEvent={event => alert(event.title)}
+                    onSelectSlot={slotInfo =>
+                        alert(
+                        `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
+                            `\nend: ${slotInfo.end.toLocaleString()}` +
+                            `\naction: ${slotInfo.action}`
+                        )
+                    }
+                    />
                 <DevTools />
             </div>
         );
